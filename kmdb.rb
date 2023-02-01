795f3a31d09ca49af4a66d272a0fe74dd8032485
mig-car <miguel.caruncho@kellogg.northwestern.edu>
2023-02-01 03:42:12 +0000

    All data inserted

--- kmdb.rb
+++ kmdb.rb
@@ -70,15 +70,122 @@
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
 # TODO!
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+Role.destroy_all
 
 # Generate models and tables, according to the domain model.
 # TODO!
+puts "The schema of the generated models/tables is:"
+puts "====================================================="
+puts "#{Studio.inspect}"
+puts "#{Movie.inspect}"
+puts "#{Actor.inspect}"
+puts "#{Role.inspect}"
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 # TODO!
 
+studio = Studio.new
+studio.name = "Warner Bros."
+studio.save
+
+wb = Studio.find_by({"name"=>"Warner Bros."})
+
+# Quick check.
+puts "Studio count: #{Studio.all.count}"
+
+movie = Movie.new
+movie.title = "Batman Begins"
+movie.year_released = 2005
+movie.rated = "PG-13"
+movie.studio_id = wb["id"]
+movie.save
+
+movie = Movie.new
+movie.title = "The Dark Knight"
+movie.year_released = 2005
+movie.rated = "PG-13"
+movie.studio_id = wb["id"]
+movie.save
+
+movie = Movie.new
+movie.title = "The Dark Knight Rises"
+movie.year_released = 2005
+movie.rated = "PG-13"
+movie.studio_id = wb["id"]
+movie.save
+
+b1 = Movie.find_by({"title" => "Batman Begins"})
+b2 = Movie.find_by({"title" => "The Dark Knight"})
+b3 = Movie.find_by({"title" => "The Dark Knight Rises"})
+
+# Quick check.
+puts "Movie count: #{Movie.all.count}"
+
+Actor.insert_all(
+    [
+        {"name" => "Christian Bale"},
+        {"name" => "Michael Caine"},
+        {"name" => "Liam Neeson"},
+        {"name" => "Katie Holmes"},
+        {"name" => "Gary Oldman"},
+        {"name" => "Heath Ledger"},
+        {"name" => "Aaron Eckhart"},
+        {"name" => "Maggie Gyllenhaal"},
+        {"name" => "Tom Hardy"},
+        {"name" => "Joseph Gordon-Levitt"},
+        {"name" => "Anne Hathaway"}
+    ]
+)
+
+cbale = Actor.find_by("name" => "Christian Bale")
+mcaine = Actor.find_by("name" => "Michael Caine")
+lneeson = Actor.find_by("name" => "Liam Neeson")
+kholmes = Actor.find_by("name" => "Katie Holmes")
+goldman = Actor.find_by("name" => "Gary Oldman")
+hledger = Actor.find_by("name" => "Heath Ledger")
+aeckhart = Actor.find_by("name" => "Aaron Eckhart")
+mg = Actor.find_by("name" => "Maggie Gyllenhaal")
+thardy = Actor.find_by("name" => "Tom Hardy")
+jgl = Actor.find_by("name" => "Joseph Gordon-Levitt")
+ahathaway = Actor.find_by("name" => "Anne Hathaway")
+
+# Quick check.
+puts "Actor count: #{Actor.all.count}"
+
+
+Role.insert_all(
+    [
+        {"movie_id" => b1["id"], "actor_id" => cbale["id"], "character_name" => "Bruce Wayne"}, 
+        {"movie_id" => b1["id"], "actor_id" => mcaine["id"], "character_name" => "Alfred"}, 
+        {"movie_id" => b1["id"], "actor_id" => lneeson["id"], "character_name" => "Ra's Al Ghul"}, 
+        {"movie_id" => b1["id"], "actor_id" => kholmes["id"], "character_name" => "Rachel Dawes"}, 
+        {"movie_id" => b1["id"], "actor_id" => goldman["id"], "character_name" => "Commissioner Gordon"},
+
+        {"movie_id" => b2["id"], "actor_id" => cbale["id"], "character_name" => "Bruce Wayne"},
+        {"movie_id" => b2["id"], "actor_id" => hledger["id"], "character_name" => "Joker"},
+        {"movie_id" => b2["id"], "actor_id" => aeckhart["id"], "character_name" => "Harvey Dent"},
+        {"movie_id" => b2["id"], "actor_id" => mcaine["id"], "character_name" => "Alfred"},
+        {"movie_id" => b2["id"], "actor_id" => mg["id"], "character_name" => "Rachel Dawes"},
+
+        {"movie_id" => b3["id"], "actor_id" => cbale["id"], "character_name" => "Bruce Wayne"},  
+        {"movie_id" => b3["id"], "actor_id" => goldman["id"], "character_name" => "Commissioner Gordon"},  
+        {"movie_id" => b3["id"], "actor_id" => thardy["id"], "character_name" => "Bane"}, 
+        {"movie_id" => b3["id"], "actor_id" => jgl["id"], "character_name" => "John Blake"},  
+        {"movie_id" => b3["id"], "actor_id" => ahathaway["id"], "character_name" => "Selina Kyle"} 
+    ]
+)
+
+# Quick check.
+puts "Role count: #{Role.all.count}"
+
+
+
 # Prints a header for the movies output
+puts "\n\n\n"
 puts "Movies"
 puts "======"
 puts ""
